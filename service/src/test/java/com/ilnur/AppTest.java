61724f3a445baecc9c61069716bcc6b25ee0cf6e
IlnurSad <sadykovilnurr@gmail.com>
2023-02-06 19:52:39 +0300

    Maven properties

---
+++ service/src/test/java/com/ilnur/AppTest.java
@@ -0,0 +1,20 @@
+package com.ilnur;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple AppRunner.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
